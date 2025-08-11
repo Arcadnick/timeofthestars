e25ec81f2391fa3690bfc2e5c8cbea526cfc51f7
Alexey <Arcadnick@yandex.ru>
2025-08-11 12:26:55 +0300

    tournament seeders

---
+++ TimeOfTheStarsServer/database/seeders/TournamentGameSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TournamentGameSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
